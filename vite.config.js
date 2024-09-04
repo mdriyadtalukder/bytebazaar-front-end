4d60684f1700f3528caf17222d4ace64597cfacd
mdriyadtalukder <riyadtalukder67@gmail.com>
2024-09-04 22:45:03 +0600

    ok

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 3000, // Set chunk size limit to 3000KB (3MB)
+  },
   server: {
     port: 3000,
   }
