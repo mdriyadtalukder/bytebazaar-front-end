0fac6479afeeabb0d3d3242bddbbecbef2dd80e6
mdriyadtalukder <riyadtalukder67@gmail.com>
2024-09-04 21:38:02 +0600

    last

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 3000, // Increase the limit as needed (e.g., 1000 kB)
+    chunkSizeWarningLimit: 2000, // Adjust the limit as needed
+    assetsInclude: ['**/*.node'], // Include .node files as assets
+    rollupOptions: {
+      external: [
+        'fs',  
+        // Externalize Node.js modules
+        'path',
+        'url',
+        'assert',
+        'child_process',
+        'module',
+      ],
+    },
   },
 })
