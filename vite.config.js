66bba2a5d25c2f066832d8c2d4821afd5c9d9f27
mdriyadtalukder <riyadtalukder67@gmail.com>
2024-09-04 22:05:51 +0600

    finish

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 2000, // Adjust the limit as needed
-    assetsInclude: ['**/*.node'], // Include .node files as assets
-    rollupOptions: {
-      external: [
-        'fs',  
-        // Externalize Node.js modules
-        'path',
-        'url',
-        'assert',
-        'child_process',
-        'module',
-      ],
-    },
-  },
+  server: {
+    port: 3000,
+  }
 })
