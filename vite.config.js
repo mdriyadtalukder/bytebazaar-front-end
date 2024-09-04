8dd206c70ee649e04c59d370c3e7dbbf281339c5
mdriyadtalukder <riyadtalukder67@gmail.com>
2024-09-04 22:51:32 +0600

    f

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
+    sourcemap: false,  // Disable source maps to reduce memory usage
     chunkSizeWarningLimit: 3000, // Set chunk size limit to 3000KB (3MB)
   },
   server: {
