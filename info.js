c3d485a640e7544e2a6d4203994adc7a86d8c4ec
mdriyadtalukder <riyadtalukder67@gmail.com>
2024-09-04 19:56:09 +0600

    finidh

--- info.js
+++ info.js
@@ -1,6 +1,8 @@
 // npm install -g npm@10.8.0
 //npm install react-image-magnify --legacy-peer-deps ...... or u can use --force instead legacy-peer-deps
 
+
+
 /* git add .
 git commit -m 'manage order'
 git push */
